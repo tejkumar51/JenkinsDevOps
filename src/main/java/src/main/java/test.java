0c43224d8e046874d0f7bfb23bf7c0c0c5e43015
tejkumar <tejkumar51@gmail.com>
2017-01-22 13:24:17 +0530

    DEVOPS-001[Tej]: Adding checkstyle to source code

---
+++ src/main/java/src/main/java/test.java
@@ -0,0 +1,17 @@
+package src.main.java;
+
+import java.util.Date;
+
+public class test {
+	private Date date = null;
+
+	public static void main(String[] args) {
+		System.out.println("*********** test class called ********");
+
+	}
+
+	public void doSomething() {
+		date = new Date();
+	}
+
+}
